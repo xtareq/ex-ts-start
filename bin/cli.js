a1cd32c974295631421288de42600a7452770786
xtareq <xtrinsic96@gmail.com>
2021-10-28 16:10:53 +0600

    added cli

---
+++ bin/cli.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node 
+
+const {execSync} = require('child_process');
+
+const runCommand = command =>{
+    try {
+        execSync(`${command}`, {stdio: 'inherit'});
+    } catch (error) {
+        console.error(`Failed to execute ${command}`, error)
+       return false;
+    }
+
+    return true 
+}
+
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/xtareq/ex-ts-start ${repoName}`;
+const installDepsCommand = `cd ${repoName} && yarn`
+
+console.log(`Cloning the repository with name ${repoName}`);
+const checkedOut = runCommand(gitCheckoutCommand);
+
+if(!checkedOut)process.exit(-1)
+
+console.log(`Installing dependencies for ${repoName}`)
+
+const installedDeps = runCommand(installDepsCommand)
+if(!installedDeps)process.exit(-1)
+
+console.log("Congratulation! You are ready to serve. Run following command to start")
+
+console.log(`cd ${repoName} && yarn dev`)
